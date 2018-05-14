0d8b6ae938fd6e6ab6d6b41d773698fe983f2ae6
RootRefresh <1779752566@qq.com>
2018-05-14 18:23:26 +0800

    二叉树遍历

---
+++ Algorith_cpp/Tree/TraverseBinaryTree.cpp
@@ -0,0 +1,77 @@
+//
+//  TraverseBinaryTree.cpp
+//  Algorith_cpp
+//
+//  Created by 丰华财经 on 2018/5/14.
+//  Copyright © 2018年 com.lottery.www. All rights reserved.
+//
+
+#include "TraverseBinaryTree.hpp"
+#include <stack>
+void preOrderTree(Tree *root)
+{
+    if (root == NULL ) {
+        return;
+    }
+    printf("%d",root->val);
+    preOrderTree(root->left);
+    preOrderTree(root->right);
+}
+
+void inOrderTree(Tree *root)
+{
+    if (root == NULL ) {
+        return;
+    }
+    inOrderTree(root->left);
+    printf("%d",root->val);
+    inOrderTree(root->right);
+}
+
+void afterOrderTree(Tree *root)
+{
+    if (root == NULL ) {
+        return;
+    }
+    afterOrderTree(root->left);
+    afterOrderTree(root->right);
+    printf("%d",root->val);
+}
+
+void stackPreOrder(Tree *root)
+{
+    stack <Tree *> myStack;
+    myStack.push(root);
+
+    while (!myStack.empty()) {
+        Tree *cur = myStack.top();
+        myStack.pop();
+        
+        printf("%d",cur->val);
+        
+        if (cur->right != NULL) {
+            myStack.push(cur->right);
+        }
+        if (cur->left != NULL) {
+            myStack.push(cur->left);
+        }
+    }
+}
+
+void stackInOrder(Tree *root)
+{
+    stack <Tree *> myStack;
+    Tree *cur = root;
+    while (!myStack.empty() || cur != NULL) {
+        if (cur != NULL) {
+            myStack.push(cur);
+            cur = cur->left;
+        }else{
+            Tree *node = myStack.top();
+            myStack.pop();
+            cur = node->right;
+            printf("%d",node->val);
+        }
+    }
+}
+
---
+++ Algorith_cpp/Tree/TraverseBinaryTree.hpp
@@ -0,0 +1,14 @@
+//
+//  TraverseBinaryTree.hpp
+//  Algorith_cpp
+//
+//  Created by 丰华财经 on 2018/5/14.
+//  Copyright © 2018年 com.lottery.www. All rights reserved.
+//
+
+#ifndef TraverseBinaryTree_hpp
+#define TraverseBinaryTree_hpp
+
+#include <stdio.h>
+#include "utils.hpp"
+#endif /* TraverseBinaryTree_hpp */
